7c88d7686d42d758b536b558f6bd1d5ca1cada1c
khizer mehdi <kmehdi85@gmail.com>
2015-04-21 14:52:39 +0530

    tst commit

--- dev_machine.sh
+++ dev_machine.sh
@@ -2,7 +2,13 @@
 
 sudo docker pull edgemehdi/elasticsearch
 sudo docker pull edgemehdi/elasticdump
-sudo docker pull edgemehdi/mongo
+sudo docker pull edgemehdi/mongodb
 sudo docker pull edgemehdi/neo4j
 sudo docker pull edgemehdi/py-environ
+
+
+#Run the ES image
+sudo docker run -d -p 9200:9200 edgemehdi/elasticsearch
+sudo docker run -d -p 27017:27017 edgemehdi/mongodb
+sudo docker run -d -p 7474:7474  edgemehdi/neo4j
 
--- py-environ/README.md
+++ py-environ/README.md
@@ -1,1 +1,7 @@
 # py-environ
+
+#Run the image
+
+sudo docker run -d -p 5000:5000 edgemehdi/py-environ
+
+#check in your browser  http://IP:5000
