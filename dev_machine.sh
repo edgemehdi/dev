9bf51d9a5ba93b8b6d0da67a31a96f675f5fa82d
khizer mehdi <kmehdi85@gmail.com>
2015-04-21 13:46:12 +0530

    Basic python  dev environ

---
+++ dev_machine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo docker pull edgemehdi/elasticsearch
+sudo docker pull edgemehdi/elasticdump
+sudo docker pull edgemehdi/mongo
+sudo docker pull edgemehdi/neo4j
+sudo docker pull edgemehdi/py-environ
+
